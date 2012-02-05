8220837090d274e3467c4096be6d0ee8107521b2
Stefan Reuter <stefan.reuter@reucon.com>
2012-02-05 02:16:03 +0100

    Additional logging

--- src/main/java/com/reucon/bamboo/CreateReleasePropertiesTask.java
+++ src/main/java/com/reucon/bamboo/CreateReleasePropertiesTask.java
@@ -81,6 +81,7 @@ public class CreateReleasePropertiesTask implements TaskType
 
     private void setVariable(TaskContext taskContext, String key, String value)
     {
+        final BuildLogger buildLogger = taskContext.getBuildLogger();
         final VariableContext variableContext = taskContext.getBuildContext().getVariableContext();
         final Map<String, VariableDefinitionContext> definitions = variableContext.getDefinitions();
 
@@ -90,9 +91,15 @@ public class CreateReleasePropertiesTask implements TaskType
             if (StringUtils.isEmpty(definition.getValue()))
             {
                 definition.setValue(value);
+                buildLogger.addBuildLogEntry(String.format("Set variable %s=%s", key, value));
+            }
+            else
+            {
+                buildLogger.addBuildLogEntry(String.format("Not setting variable %s because it is already set", key));
             }
             return;
         }
         customVariableContext.addCustomData(key, value);
+        buildLogger.addBuildLogEntry(String.format("Set custom variable %s=%s", key, value));
     }
 }
